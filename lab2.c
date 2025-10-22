093723578b955149b643747a9750970f82f20663
taekang1117 <110898265+taekang1117@users.noreply.github.com>
2025-10-21 17:47:56 -0700

    Update lab2.c

--- lab2.c
+++ lab2.c
@@ -19,31 +19,48 @@ void main(void) {
     // 2. GPIO SETUP
     PORTA_PCR1 = 0xA0100; // Configure Port A pin 1 for GPIO Interrupt on falling edge    
     PORTB_GPCLR = 0x000C0100; // GPIO Setup: PORTB pin 2 and 3
-    PORTC_GPCLR = 0x01BF0100 // GPIO Setup: LED1 8 pins 0,1,2,3,4,5,7
-    PORTD_GPCLR = 0x00FF0100 // GPIO Setup: LED2 8 pins 
+    PORTC_GPCLR = 0x01BF0100; // GPIO Setup: LED1 8 pins 0,1,2,3,4,5,7
+    PORTD_GPCLR = 0x00FF0100; // GPIO Setup: LED2 8 pins 
+    
+    // 3. ADC Configuration
     ADC0_CFG1 = 0x0C; // 16 bits ADC, Bus Clock
     ADC0_SC1A = 0x1F; // Disable the module during init, ADCH = 11111
 
-    // 3. GPIO DIRECTION
-    GPIOB_PDDR = 0x00000000// DIP Switch 
-    GPIOC_PDDR = 0x000001BF// LED1
-    GPIOD_PDDR = 0x000000FF// LED2
+    // 4. GPIO DIRECTION
+    GPIOB_PDDR = 0x00000000; // DIP Switch 
+    GPIOC_PDDR = 0x000001BF; // LED1
+    GPIOD_PDDR = 0x000000FF; // LED2
     
-
-    PORTA_ISFR = (1<<1); // Clear ISFR for Port A pin 1
-    NVIC_EnableIRQ(PORTA_IRQn); // Enable Port A interrupt in
-
-    // 4. High Low
+    // 5. INITIAL OUTPUT STATE
     GPIOB_PDOR = 0x0000000C; // Set Pins 2 and 3 on Port B to be high voltage
     GPIOC_PDOR = 0x00000000; // Set Pins 0-5, 7-8 on Port C to be low voltage
     GPIOD_PDOR = 0x000000FF; // Set Pins 0-7 on Port D to be low voltage
-    
- // TO DO: Main code loop
-    while(1) {
-        
-}
 
+    // 6. INTERRUPT CONFIG
+    PORTA_ISFR = (1<<1); // Clear ISFR for Port A pin 1
+    NVIC_EnableIRQ(PORTA_IRQn); // Enable Port A interrupt in
+
+    // TO DO: 7. MAIN LOOP
+    while(1) {
+        // Read Switch State
+        unit32_t mode_sw = (GPIOB_PDIR >> 2) & 0x01;
+        unit32_t cnt_dir = (GPIOB_PDIR >> 3) & 0x01;
+
+        if (mode_sw == 0) {
+            // Mode 0 : Read Potentiometer and display
+            unit16_t adc_val = ADC_read16b();
+            // display adc_val to LEDs;
+        } else {
+            // Mode 1: Counter Mode
+            if (cnt_dir == 0) {
+                // Count Upward
+            } else {
+                // Count Donward
+            }
+        }
+    }
+}
 // TO DO: void PORTA_IRQHandler(void) 
-unsigned 
+
 // TO DO: Interrupt Code
 
