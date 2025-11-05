72b489158ead0c45ec686a2f0450bd917a4b1c72
taekang1117 <110898265+taekang1117@users.noreply.github.com>
2025-11-04 19:28:03 -0800

    Update main.c

--- lab4/main.c
+++ lab4/main.c
@@ -1,1 +1,39 @@
-main
+#include "fsl_device_registers.h"
+
+int nums[10] = {0b1111110, // 0
+0b0110000, // 1
+0b1101101, // 2
+0b1111001, // 3
+0b0110011, // 4
+0b1011011, // 5
+0b1011111, // 6
+0b1110000, // 7
+0b1111111, // 8
+0b1111011, // 9
+};
+
+void IRQ_Handler(void)
+{
+    SIM_SCGC5 |= SIM_SCGC5_PORTD_MASK; /*Enable Port D Clock Gate Control*/
+    SIM_SCGC5 |= SIM_SCGC5_PORTC_MASK; /*Enable Port C Clock Gate Control*/
+    SIM_SCGC5 |= SIM_SCGC5_PORTA_MASK;
+    SIM_SCGC5 |= SIM_SCGC5_PORTB_MASK; /*Enable Port A Clock Gate Control*/
+
+  
+}
+
+
+int main(void) 
+{
+// clock gate
+  // set GPIO
+// set input
+  
+
+
+// NVIC En
+}
+
+
+
+
