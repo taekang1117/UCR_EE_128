c69ecc1830c912e95551d251383223a8011d26e5
taekang1117 <110898265+taekang1117@users.noreply.github.com>
2025-11-30 19:24:02 -0800

    Update testcode01.c

--- Final/testcode01.c
+++ Final/testcode01.c
@@ -1,34 +1,34 @@
-int main(void)
-{
-    init_gpio();
-    init_systick();
+// int main(void)
+// {
+//     init_gpio();
+//     init_systick();
 
-    // Start in COUNTDOWN right away
-    currentState      = STATE_COUNTDOWN;
-    seconds_left      = 9;
-    countdown_ms      = 9000;
-    countdown_active  = 1;
-    displayFlag       = 1;
+//     // Start in COUNTDOWN right away
+//     currentState      = STATE_COUNTDOWN;
+//     seconds_left      = 9;
+//     countdown_ms      = 9000;
+//     countdown_active  = 1;
+//     displayFlag       = 1;
 
-    red_off();
-    green_on();
+//     red_off();
+//     green_on();
 
-    while (1) {
-        if (timeoutFlag) {
-            timeoutFlag = 0;
-            // When timer finishes, go back to locked display 9/red on
-            currentState      = STATE_LOCKED;
-            countdown_active  = 0;
-            countdown_ms      = 0;
-            seconds_left      = 9;
-            displayFlag       = 1;
-            red_on();
-            green_off();
-        }
+//     while (1) {
+//         if (timeoutFlag) {
+//             timeoutFlag = 0;
+//             // When timer finishes, go back to locked display 9/red on
+//             currentState      = STATE_LOCKED;
+//             countdown_active  = 0;
+//             countdown_ms      = 0;
+//             seconds_left      = 9;
+//             displayFlag       = 1;
+//             red_on();
+//             green_off();
+//         }
 
-        if (displayFlag) {
-            displayFlag = 0;
-            display_digit(seconds_left % 10);
-        }
-    }
-}
+//         if (displayFlag) {
+//             displayFlag = 0;
+//             display_digit(seconds_left % 10);
+//         }
+//     }
+// }
